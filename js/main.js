57b8709dba6749961899da8c51c4e38e7ab90a53
Richard Davis <rdavis.bacs@gmail.com>
2013-09-30 13:34:47 -0400

    working

--- js/main.js
+++ js/main.js
@@ -1,47 +1,34 @@
-$('li > a').click(function() {
-  $(this).toggleClass('on');
-});
-
-$('#all-on').click(function() {
-  $('li > a').addClass('on');
-});
-
-$('#all-off').click(function() {
-  $('li > a').removeClass('on');
-});
-
-$('#all-exteriors-on').click(function() {
-  $('.exterior > a').addClass('on');
-});
-
-$('#all-exteriors-off').click(function() {
-  $('.exterior > a').removeClass('on');
-});
-
-$('#all-interiors-on').click(function() {
-  $('.interior > a').addClass('on');
-});
-
-$('#all-interiors-off').click(function() {
-  $('.interior > a').removeClass('on');
-});
-
-$('#all-bathroom-on').click(function() {
-  $('.bathroom > a').addClass('on');
-});
-
-$('#all-bathroom-off').click(function() {
-  $('.bathroom > a').removeClass('on');
-});
-
 function updateCount() {
   var lightsOn = $('.on').length;
   var totalLights = $('.lighting-list a').length;
-
-  $('lights-on-total strong').text(lightsOn);
-  $('lights-off-total strong').text(totalLights - lightsOn);
+  $('#lights-on-total strong').text(lightsOn);
+  $('#lights-off-total strong').text(totalLights - lightsOn);
 }
 
 $(document).ready(function() {
   updateCount();
+
+  // handle clicking on lights matrix
+  $('.matrix li a').bind('click', function(e) {
+    $(this).toggleClass('on');
+    updateCount();
+  });
+
+  // handle clicking on light buttons
+  $('.actions input').bind('click', function(e) {
+    var command = this.id.split('-');
+    var selector = '.lighting-list';
+
+    if (command.length === 3) {
+      selector += ' .' + command[1];
+    }
+    selector += ' a';  // selector = '.lighting-list .interior a'
+
+    if (command[command.length - 1] === 'on') {
+      $(selector).addClass('on');
+    } else { // command.last === 'off'
+      $(selector).removeClass('on');
+    }
+    updateCount();
+  });
 });
